5e4998a812c1dd4edffacd682bb6c3486e74e086
iamdeepanjan <deepanjanbmp3@gmail.com>
2024-12-08 00:11:16 +0530

    backend added

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+const dotenv = require('dotenv');
+
+dotenv.config({path:"./config/config.env"});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
+})
